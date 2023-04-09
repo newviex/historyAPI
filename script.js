e985166a8b51cb75c566e60274e69a82750d9c38
newviex <xaldrienallanigue@gmail.com>
2023-04-09 10:14:41 +0800

    explain window alert and alert example

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-alert(history.length);
+window.alert('this is the first window.alert'+history.length + ' value for the history.length!');
+
+alert('this is the first'+history.length + ' window object is implied!');
